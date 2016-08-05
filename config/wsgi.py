bab9af91abe6728457b09093670dad10c81b99e9
florianm <florian.mayer@dpaw.wa.gov.au>
2016-08-05 10:56:08 +0800

    wsgi changes

--- config/wsgi.py
+++ config/wsgi.py
@@ -15,7 +15,7 @@ framework.
 """
 import confy
 import os
-from dj_static import Cling, MediaCling
+from dj_static import Cling   # , MediaCling
 from django.core.wsgi import get_wsgi_application
 # from whitenoise import WhiteNoise
 # from config.settings.production import STATIC_ROOT
@@ -27,7 +27,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production")
 # application.add_files('/path/to/more/static/files', prefix='more-files/')
 
 
-application = Cling(MediaCling(get_wsgi_application()))
+application = Cling(get_wsgi_application())
 
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication
