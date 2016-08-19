6176f7cc4e0400a316477ba2a83c01091f16aea6
florianm <florian.mayer@dpaw.wa.gov.au>
2016-08-19 12:43:56 +0800

    added functools to debug circleci

--- functools.py
+++ functools.py
@@ -52,20 +52,20 @@ def wraps(wrapped,
 
 def total_ordering(cls):
     """Class decorator that fills in missing ordering methods"""
-    convert = {
-        '__lt__': [('__gt__', lambda self, other: not (self < other or self == other)),
-                   ('__le__', lambda self, other: self < other or self == other),
-                   ('__ge__', lambda self, other: not self < other)],
-        '__le__': [('__ge__', lambda self, other: not self <= other or self == other),
-                   ('__lt__', lambda self, other: self <= other and not self == other),
-                   ('__gt__', lambda self, other: not self <= other)],
-        '__gt__': [('__lt__', lambda self, other: not (self > other or self == other)),
-                   ('__ge__', lambda self, other: self > other or self == other),
-                   ('__le__', lambda self, other: not self > other)],
-        '__ge__': [('__le__', lambda self, other: (not self >= other) or self == other),
-                   ('__gt__', lambda self, other: self >= other and not self == other),
-                   ('__lt__', lambda self, other: not self >= other)]
-    }
+     convert = {
+    '__lt__': [('__gt__', lambda self, other: not (self < other or self == other)),
+               ('__le__', lambda self, other: self < other or self == other),
+               ('__ge__', lambda self, other: not self < other)],
+    '__le__': [('__ge__', lambda self, other: not self <= other or self == other),
+               ('__lt__', lambda self, other: self <= other and not self == other),
+               ('__gt__', lambda self, other: not self <= other)],
+    '__gt__': [('__lt__', lambda self, other: not (self > other or self == other)),
+               ('__ge__', lambda self, other: self > other or self == other),
+               ('__le__', lambda self, other: not self > other)],
+    '__ge__': [('__le__', lambda self, other: (not self >= other) or self == other),
+               ('__gt__', lambda self, other: self >= other and not self == other),
+               ('__lt__', lambda self, other: not self >= other)]
+               }
     roots = set(dir(cls)) & set(convert)
     if not roots:
         raise ValueError('must define at least one ordering operation: < > <= >=')
