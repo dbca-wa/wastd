ce8c3d0e998f30c97a0e9712634e7bde0882d420
florianm <florian.mayer@dpaw.wa.gov.au>
2016-10-03 14:42:54 +0800

    error pages

--- config/urls.py
+++ config/urls.py
@@ -87,22 +87,18 @@ urlpatterns = [
     # Update animal names for all Encounters
     url(r'^action/update-names/$', update_names, name="update-names"),
 
+    url(r'^400/$', default_views.bad_request,
+        kwargs={'exception': Exception('Bad Request!')}),
+
+    url(r'^403/$', default_views.permission_denied,
+        kwargs={'exception': Exception('Permission Denied')}),
+
+    url(r'^404/$', default_views.page_not_found,
+        kwargs={'exception': Exception('Page not Found')}),
+
+    url(r'^500/$', default_views.server_error,
+        kwargs={'exception': Exception('Infernal Server Error')}),
+
     ] +\
     static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) +\
     staticfiles_urlpatterns()
-
-if settings.DEBUG:
-    # This allows the error pages to be debugged during development, just visit
-    # these url in browser to see how these error pages look like.
-    urlpatterns += [
-        url(r'^400/$', default_views.bad_request,
-            kwargs={'exception': Exception('Bad Request!')}),
-
-        url(r'^403/$', default_views.permission_denied,
-            kwargs={'exception': Exception('Permission Denied')}),
-
-        url(r'^404/$', default_views.page_not_found,
-            kwargs={'exception': Exception('Page not Found')}),
-
-        url(r'^500/$', default_views.server_error),
-        ]
