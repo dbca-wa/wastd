085385f51eaa4b0b0a051eb1d50f2bd3892f3d87
florianm <florian.mayer@dpaw.wa.gov.au>
2017-03-17 14:44:33 +0800

    show name in EncounterAdmin and lower classes - undo

--- wastd/observations/admin.py
+++ wastd/observations/admin.py
@@ -339,7 +339,7 @@ class EncounterAdmin(FSMTransitionMixin, VersionAdmin, admin.ModelAdmin):
     # Change_view form layout
     fieldsets = (('Encounter', {'fields': (
         'site_visit', 'where', 'location_accuracy', 'when',
-        'observer', 'reporter', 'source', 'source_id', 'name',)}),)
+        'observer', 'reporter', 'source', 'source_id', )}),)
 
     # Change_view inlines
     inlines = [
