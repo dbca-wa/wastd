85943666172cbbf4ee5fd62a36cc0b591b66a085
Florian Mayer <Florian.Mayer@dpaw.wa.gov.au>
2018-08-03 12:32:49 +0800

    UAT deployment using supervisor and gunicorn

--- manage.py
+++ manage.py
@@ -7,6 +7,6 @@ import confy
 confy.read_environment_file()
 
 if __name__ == '__main__':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.local')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.production')
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
