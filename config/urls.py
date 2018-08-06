72ae7279f32403042b78b95a732a628932f00ee3
florianm <florian.mayer@dpaw.wa.gov.au>
2018-08-06 11:36:12 +0800

    urls: fix debug urls

--- config/urls.py
+++ config/urls.py
@@ -3,7 +3,7 @@
 from __future__ import unicode_literals
 
 from django.conf import settings
-from django.urls import path, re_path, include
+from django.urls import re_path, include
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.admin import site
@@ -119,11 +119,11 @@ urlpatterns = [
             kwargs={'exception': Exception('Page not Found')}),
 
     # url(r'^500/$', default_views.server_error,
-    #     kwargs={'exception': Exception('Infernal Server Error')}),
+    #     kwargs={'exception': Exception('Internal Server Error')}),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) +\
     staticfiles_urlpatterns()
 
 if settings.DEBUG:
     import debug_toolbar
-    urlpatterns += [path(r'^__debug__/', include(debug_toolbar.urls)), ]
+    urlpatterns += [re_path(r'^__debug__/', include(debug_toolbar.urls)), ]
