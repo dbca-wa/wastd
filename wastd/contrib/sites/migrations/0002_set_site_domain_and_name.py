92da8045c5920b4ce870216426bc481db87731ef
florianm <florian.mayer@dpaw.wa.gov.au>
2018-08-06 14:20:28 +0800

    CI: re-enable coveralls, preserve logs

--- wastd/contrib/sites/migrations/0002_set_site_domain_and_name.py
+++ wastd/contrib/sites/migrations/0002_set_site_domain_and_name.py
@@ -4,7 +4,6 @@ To understand why this file is here, please read:
 
 http://cookiecutter-django.readthedocs.io/en/latest/faq.html#why-is-there-a-django-contrib-sites-directory-in-cookiecutter-django
 """
-# -*- coding: utf-8 -*-
 
 from __future__ import unicode_literals
 
