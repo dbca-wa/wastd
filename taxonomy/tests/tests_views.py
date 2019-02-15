9d358f20b6d69d06fb3368efd304270824310730
florianm <florian.mayer@dpaw.wa.gov.au>
2019-02-15 15:46:37 +0800

    Add to taxonomy tests #104

--- taxonomy/tests/tests_views.py
+++ taxonomy/tests/tests_views.py
@@ -29,7 +29,7 @@ MOMMY_CUSTOM_FIELDS_GEN = MOMMY_SPATIAL_FIELDS
 from taxonomy.models import Community, Taxon  # noqa
 
 
-class CommunityTests(TestCase):
+class CommunityViewTests(TestCase):
     """Community tests."""
 
     def setUp(self):
@@ -74,7 +74,7 @@ class CommunityTests(TestCase):
 
     def test_com_detail_url_loads(self):
         """Test Community detail_url."""
-        url = reverse('community-detail', kwargs={'pk': self.cae.community.pk})
+        url = reverse('community-detail', kwargs={'pk': self.com.pk})
         self.assertEqual(url, self.com.detail_url)
 
         response = self.client.get(self.com.detail_url)
@@ -82,8 +82,68 @@ class CommunityTests(TestCase):
 
     def test_com_update_url_loads(self):
         """Test Community update_url."""
-        url = reverse('community-update', kwargs={'pk': self.cae.community.pk})
+        url = reverse('community-update', kwargs={'pk': self.com.pk})
         self.assertEqual(url, self.com.update_url)
 
         response = self.client.get(self.com.update_url)
+        self.assertEqual(response.status_code, 200)
+
+
+class TaxonViewTests(TestCase):
+    """Taxon view tests."""
+
+    def setUp(self):
+        """Shared objects."""
+        self.com = mommy.make(
+            Community,
+            code="code0",
+            name="name0",
+            _fill_optional=['eoo'])
+        self.com0.save()
+
+        self.com1 = mommy.make(
+            Community,
+            code="code1",
+            name="name1",
+            _fill_optional=['eoo'])
+        self.com1.save()
+
+        self.taxon = mommy.make(
+            Taxon,
+            name_id=1000,
+            name="name0",
+            _fill_optional=['rank', 'eoo'])
+        self.taxon0.save()
+
+        self.user = get_user_model().objects.create_superuser(
+            username="superuser",
+            email="super@gmail.com",
+            password="test")
+        self.user.save()
+
+        self.client.force_login(self.user)
+
+    def test_taxon_creation(self):
+        """Test creating a Taxon."""
+        self.assertTrue(isinstance(self.taxon, Taxon))
+
+    def test_taxon_absolute_admin_url_loads(self):
+        """Test Taxon absolute_admin_url."""
+        response = self.client.get(self.taxon.absolute_admin_url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_taxon_detail_url_loads(self):
+        """Test Taxon detail_url."""
+        url = reverse('taxon-detail', kwargs={'name_id': self.taxon.name_id})
+        self.assertEqual(url, self.taxon.detail_url)
+
+        response = self.client.get(self.taxon.detail_url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_taxon_update_url_loads(self):
+        """Test Taxon update_url."""
+        url = reverse('taxon-update', kwargs={'name_id': self.taxon.name_id})
+        self.assertEqual(url, self.taxon.update_url)
+
+        response = self.client.get(self.taxon.update_url)
         self.assertEqual(response.status_code, 200)
