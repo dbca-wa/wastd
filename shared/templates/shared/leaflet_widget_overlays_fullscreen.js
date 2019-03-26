9d037c2d9d27eb2d46b897db6939f542684c82c8
florianm <florian.mayer@dpaw.wa.gov.au>
2019-03-26 08:17:54 +0800

    REQ 16: add geocoder to map widget.

--- shared/templates/shared/leaflet_widget_overlays_fullscreen.js
+++ shared/templates/shared/leaflet_widget_overlays_fullscreen.js
@@ -1,7 +1,9 @@
+<script src="https://unpkg.com/leaflet-control-geocoder/dist/Control.Geocoder.js"></script>
 <script type="text/javascript">
   window.addEventListener("map:init", function (event) {
     var map = event.detail.map;
     {% include 'shared/overlays.html' %}
     map.addControl(new L.Control.Fullscreen());
+    map.addControl(new L.Control.Geocoder());
 });
 </script>
