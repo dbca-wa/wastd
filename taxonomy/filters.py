7c22a8b66e47ed4a02440e7b72f548c0b0bb4f81
florianm <florian.mayer@dpaw.wa.gov.au>
2019-04-10 07:42:34 +0800

    #145 Filter Taxon/Com by DBCA reg/dist solved

--- taxonomy/filters.py
+++ taxonomy/filters.py
@@ -4,12 +4,16 @@
 import django_filters
 from conservation.models import ConservationCategory
 from django.contrib.gis.db import models as geo_models
-from django_filters.filters import BooleanFilter, ModelMultipleChoiceFilter  # noqa
+from django.contrib.gis.db.models import Extent, Union, Collect  # noqa
+from django_filters.filters import (
+    BooleanFilter, ModelChoiceFilter, ModelMultipleChoiceFilter)
 from django_filters.widgets import BooleanWidget  # noqa
 
 # from django import forms
-from .models import Community, Taxon
+from taxonomy.models import Community, Taxon
+from occurrence import models as occ_models
 from shared.filters import FILTER_OVERRIDES
+from wastd.observations.models import Area
 
 
 class TaxonFilter(django_filters.FilterSet):
@@ -24,7 +28,7 @@ class TaxonFilter(django_filters.FilterSet):
         label="Taxonomic name is current",
         widget=BooleanWidget()
     )
-    taxon_gazettal__category = ModelMultipleChoiceFilter(
+    taxon_gazettal__category = ModelChoiceFilter(
         label="Conservation listed as",
         queryset=ConservationCategory.objects.filter(
             conservation_list__scope_species=True
@@ -35,6 +39,15 @@ class TaxonFilter(django_filters.FilterSet):
         )
     )
     eoo = geo_models.PolygonField()
+    admin_areas = ModelMultipleChoiceFilter(
+        label="DBCA Regions and Districts",
+        queryset=Area.objects.filter(
+            area_type__in=[
+                Area.AREATYPE_LOCALITY,  # until reg/dist are populated
+                Area.AREATYPE_DBCA_REGION,
+                Area.AREATYPE_DBCA_DISTRICT]),
+        method='taxa_occurring_in_area'
+    )
 
     class Meta:
         """Class opts."""
@@ -42,6 +55,7 @@ class TaxonFilter(django_filters.FilterSet):
         model = Taxon
         fields = [
             "paraphyletic_groups",
+            "admin_areas",
             "eoo",
             "taxon_gazettal__category",
             "taxonomic_name",
@@ -59,6 +73,32 @@ class TaxonFilter(django_filters.FilterSet):
         """Return terminal taxa (leaf nodes) if value is true."""
         return queryset.filter(children__isnull=value)
 
+    def taxa_occurring_in_area(self, queryset, name, value):
+        """Return Taxa occurring in the given Area.
+
+        * The filter returns a list of Area objects as ``value``
+        * We need to extract their PKs to create a queryset equivalent to
+          the list of objects ``value``. Only querysets allow agggregation, not lists.
+        * A search_area Multipolygon is collected from the geoms of Areas in ``value``
+        * The Taxon PKs are calculated from occurrences (TaxonAreaEncounters)
+          ``intersect``ing the search_area
+        * The queryset is filtered by the list of Taxon PKs with occurrences
+        """
+        if value:
+            area_pks = [area.pk for area in value]
+            search_area = Area.objects.filter(
+                pk__in=area_pks
+            ).aggregate(Collect('geom'))["geom__collect"]
+            taxon_pks_in_area = set(
+                [x["taxon__pk"]
+                 for x in occ_models.TaxonAreaEncounter.objects.filter(
+                    point__intersects=search_area
+                ).values("taxon__pk")]
+            )
+            return queryset.filter(pk__in=taxon_pks_in_area)
+        else:
+            return queryset
+
 
 class CommunityFilter(django_filters.FilterSet):
     """Filter for Community."""
@@ -74,17 +114,52 @@ class CommunityFilter(django_filters.FilterSet):
         )
     )
     eoo = geo_models.PolygonField()
+    admin_areas = ModelMultipleChoiceFilter(
+        label="DBCA Regions and Districts",
+        queryset=Area.objects.filter(
+            area_type__in=[
+                Area.AREATYPE_LOCALITY,  # until reg/dist are populated
+                Area.AREATYPE_DBCA_REGION,
+                Area.AREATYPE_DBCA_DISTRICT]),
+        method='communities_occurring_in_area'
+    )
 
     class Meta:
         """Class opts."""
 
         model = Community
         fields = [
+            "admin_areas",
             "eoo",
             "community_gazettal__category",
             "code",
             "name",
             "description",
         ]
-        # widgets = {"eoo": LeafletWidget()}
         filter_overrides = FILTER_OVERRIDES
+
+    def communities_occurring_in_area(self, queryset, name, value):
+        """Return Communities occurring in the given Area.
+
+        * The filter returns a list of Area objects as ``value``
+        * We need to extract their PKs to create a queryset equivalent to
+          the list of objects ``value``. Only querysets allow agggregation, not lists.
+        * A search_area Multipolygon is collected from the geoms of Areas in ``value``
+        * The Taxon PKs are calculated from occurrences (CommunityAreaEncounters)
+          ``intersect``ing the search_area
+        * The queryset is filtered by the list of Community PKs with occurrences
+        """
+        if value:
+            area_pks = [area.pk for area in value]
+            search_area = Area.objects.filter(
+                pk__in=area_pks
+            ).aggregate(Collect('geom'))["geom__collect"]
+            com_pks_in_area = set(
+                [x["community__pk"]
+                 for x in occ_models.CommunityAreaEncounter.objects.filter(
+                    point__intersects=search_area
+                ).values("community__pk")]
+            )
+            return queryset.filter(pk__in=com_pks_in_area)
+        else:
+            return queryset
