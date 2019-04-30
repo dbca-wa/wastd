8aad853322c8d14d33e5880fc84df76838413110
florianm <florian.mayer@dpaw.wa.gov.au>
2019-04-30 18:18:27 +0800

    Occurrence template tags to templates

--- occurrence/templatetags/occurrence_tags.py
+++ occurrence/templatetags/occurrence_tags.py
@@ -1,83 +1,14 @@
 # -*- coding: utf-8 -*-
 """Template tags for occurrence."""
 # import json
-
-
 # import django
 # from django.utils.encoding import force_str
 from django import template
 # from django.contrib.admin.util import lookup_field, quote
 # from django.conf import settings
-from django.urls import reverse  # resolve
+# from django.urls import reverse  # resolve
 from shared.utils import sanitize_tag_label
 
 register = template.Library()
 
 register.filter('sanitize_tag_label', sanitize_tag_label)
-
-# @register.inclusion_tag('occurrence/include/admin_areaencounter_create_link.html', takes_context=False)
-# def admin_areaencounter_create_link(pk, subject, block=False, label=False):
-#     """Render an "add occurrence" link for staff."""
-#     return {
-#         "pk": pk,
-#         "subject": subject,
-#         "admin_add_url": "{0}?{1}={2}".format(
-#             reverse('admin:occurrence_{0}areaencounter_add'.format(subject)),
-#             subject,
-#             pk
-#         ),
-#         "block": block,
-#         "label": label
-#     }
-
-
-@register.inclusion_tag('occurrence/include/admin_areaencounter_list_link.html', takes_context=False)
-def admin_areaencounter_list_link(pk, subject, block=False, label=False):
-    """Render an "list subject area occurrences" link for a subject."""
-    return {
-        "list_url": reverse('admin:occurrence_{0}areaencounter_changelist'.format(subject)) +
-        "?{0}__id__exact={1}".format(subject, pk),
-        "pk": pk,
-        "subject": subject,
-        "block": block,
-        "label": label
-    }
-
-
-# @register.inclusion_tag('occurrence/include/areaencounter_add_link.html', takes_context=False)
-# def taxonareaencounter_add_link(name_id, area_code=None, block=False, label=False):
-#     """Render an "add taxon area occurrence" link for a taxon."""
-#     if area_code:
-#         add_url = reverse('occurrence:taxon-occurrence-area-create',
-#                           kwargs={'name_id': name_id,
-#                                   'area_code': sanitize_tag_label(area_code)})
-#     else:
-#         add_url = reverse("occurrence:taxon-occurrence-create",
-#                           kwargs={"name_id": name_id})
-
-#     return {
-#         "add_url": add_url,
-#         "subject": "taxon",
-#         "area_code": sanitize_tag_label(area_code),
-#         "block": block,
-#         "label": label
-#     }
-
-
-# @register.inclusion_tag('occurrence/include/areaencounter_add_link.html', takes_context=False)
-# def communityareaencounter_add_link(pk, area_code=None, block=False, label=False):
-#     """Render an "add community area occurrence" link for a community."""
-#     if area_code:
-#         add_url = reverse('occurrence:community-occurrence-area-create',
-#                           kwargs={'pk': pk, 'area_code': sanitize_tag_label(area_code)})
-#     else:
-#         add_url = reverse('occurrence:community-occurrence-create',
-#                           kwargs={'pk': pk})
-
-#     return {
-#         "add_url": add_url,
-#         "subject": "community",
-#         "area_code": sanitize_tag_label(area_code),
-#         "block": block,
-#         "label": label
-#     }
