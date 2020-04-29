db7ca0620d9429d2b13301494f9e77a985d268a0
Ashley Felton <ashley@ropable.com>
2020-04-29 12:29:27 +0800

    Bugfix edge case of category/criteria being absent for taxon-conservationlisting endpoint.

--- conservation/api.py
+++ conservation/api.py
@@ -254,11 +254,15 @@ class TaxonConservationListingViewSet(BatchUpsertViewSet):
             cat_ids = force_as_list(update_data.pop("category"))
             logger.debug("[API][create_one] Found categories {}".format(cat_ids))
             categories = [ConservationCategory.objects.get(id=x) for x in cat_ids if x != 'NA']
+        else:
+            categories = []
 
         if 'criteria' in update_data:
             crit_ids = force_as_list(update_data.pop("criteria"))
             logger.debug("[API][create_one] Found criteria {}".format(crit_ids))
             criteria = [ConservationCriterion.objects.get(id=x) for x in crit_ids if x != 'NA']
+        else:
+            criteria = []
 
         # Early exit 1: None value in unique data
         if None in unique_data.values():
@@ -274,8 +278,10 @@ class TaxonConservationListingViewSet(BatchUpsertViewSet):
 
         # Without the QA status deciding whether to update existing data:
         obj, created = self.model.objects.update_or_create(defaults=update_data, **unique_data)
-        obj.category.set(categories)  # TODO: must do individually or can do as list?
-        obj.criteria.set(criteria)
+        if categories:
+            obj.category.set(categories)  # TODO: must do individually or can do as list?
+        if criteria:
+            obj.criteria.set(criteria)
         obj.save()  # better save() than sorry
         obj.refresh_from_db()
         obj.save()  # to update cached fields
