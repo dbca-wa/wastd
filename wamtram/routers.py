de2cc3bb13680f8a4c07e1311cbed3c130eb9724
Ashley Felton <ashley@ropable.com>
2023-01-17 08:37:11 +0800

    Bugfix wamtram/routers.py

--- wamtram/routers.py
+++ wamtram/routers.py
@@ -11,11 +11,11 @@ class WamtramRouter:
         return None
 
     def allow_relation(self, obj1, obj2, **hints):
-        if obj1._meta.app_label  == 'wamtram' or obj2._meta.app_label == 'wamtram':
-           return True
+        if obj1._meta.app_label == 'wamtram' or obj2._meta.app_label == 'wamtram':
+            return True
         return None
 
     def allow_migrate(self, db, app_label, model_name=None, **hints):
-        if model._meta.app_label == 'wamtram':
+        if app_label == 'wamtram':
             return None
         return None
