a27a4d5f999ad01aade703a26870f1f9b7542aab
Ashley Felton <ashley@ropable.com>
2023-03-02 15:18:22 +0800

    Simplify filtering for users.

--- users/filters.py
+++ users/filters.py
@@ -1,43 +1,14 @@
-"""Filters for WAStD Users."""
 from django_filters import FilterSet
-from django_filters.filters import (  # noqa
-    BooleanFilter,
-    CharFilter,
-    RangeFilter,
-    ChoiceFilter,
-    MultipleChoiceFilter,
-    ModelChoiceFilter,
-    ModelMultipleChoiceFilter,
-)
-from shared.filters import FILTER_OVERRIDES
 from users.models import User
 
 
 class UserFilter(FilterSet):
-    """User Filter.
-
-    https://django-filter.readthedocs.io/en/latest/usage.html
-    """
 
     class Meta:
-        """Options for UserFilter."""
-
         model = User
-        filter_overrides = FILTER_OVERRIDES
-        fields = [
-            "username",
-            "name",
-            "nickname",
-            "aliases",
-            "role",
-            "email",
-            "phone",
-            "affiliation",
-            # 'organisations',
-            "is_staff",
-            "is_active",
-            "date_joined",
-            "last_login",
-            "is_superuser",
-            "alive",
-        ]
+        fields = {
+            "name": ["icontains"],
+            "email": ["icontains"],
+            "phone": ["contains"],
+            "is_active": ["exact"],
+        }
