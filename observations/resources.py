b92223779bb0d97b0b7a92eec0c4851a290bf00f
Ashley Felton <ashley@ropable.com>
2023-03-07 14:21:56 +0800

    Refactor TurtleNestEncounterResource to return a subset of columns, with nicer headings.

--- observations/resources.py
+++ observations/resources.py
@@ -1,4 +1,7 @@
+from import_export.fields import Field
 from import_export.resources import ModelResource
+
+from .lookups import NA_VALUE
 from .models import (
     Encounter,
     AnimalEncounter,
@@ -38,24 +41,47 @@ class SurveyResource(ModelResource):
 
 
 class EncounterResource(ModelResource):
+
+    locality = Field(column_name='locality')
+    site = Field(column_name='site')
+    observer = Field(column_name='observer')
+    reporter = Field(column_name='reporter')
+
+    def dehydrate_locality(self, encounter):
+        return encounter.area.name if encounter.area else ''
+
+    def dehydrate_site(self, encounter):
+        return encounter.site.name if encounter.site else ''
+
+    def dehydrate_observer(self, encounter):
+        return encounter.observer.name
+
+    def dehydrate_reporter(self, encounter):
+        return encounter.reporter.name
+
+    def dehydrate_encounter_type(self, encounter):
+        return encounter.get_encounter_type_display()
+
     class Meta:
         model = Encounter
         fields = [
-            "source",
-            "source_id",
-            "where",
-            "wkt" "longitude",
-            "latitude",
-            "location_accuracy_m",
+            "id",
             "when",
-            "area",
-            "area__name",
+            "where",
+            "locality",
             "site",
-            "site__name",
             "observer",
-            "observer__name",
             "reporter",
-            "reporter__name",
+            "encounter_type",
+        ]
+        export_order = [
+            "id",
+            "when",
+            "where",
+            "locality",
+            "site",
+            "observer",
+            "reporter",
             "encounter_type",
         ]
 
@@ -104,30 +130,59 @@ class AnimalEncounterResource(ModelResource):
         ]
 
 
-class TurtleNestEncounterResource(ModelResource):
+class TurtleNestEncounterResource(EncounterResource):
+
+    def dehydrate_nest_type(self, encounter):
+        return encounter.get_nest_type_display()
+
+    def dehydrate_nest_age(self, encounter):
+        return encounter.get_nest_age_display()
+
+    def dehydrate_species(self, encounter):
+        return encounter.get_species_display()
+
+    def dehydrate_disturbance(self, encounter):
+        if encounter.disturbance == NA_VALUE:
+            return ''
+        else:
+            return encounter.get_disturbance_display()
+
+    def dehydrate_nest_tagged(self, encounter):
+        if encounter.nest_tagged == NA_VALUE:
+            return ''
+        else:
+            return encounter.get_nest_tagged_display()
+
+    def dehydrate_logger_found(self, encounter):
+        if encounter.logger_found == NA_VALUE:
+            return ''
+        else:
+            return encounter.get_logger_found_display()
+
+    def dehydrate_eggs_counted(self, encounter):
+        if encounter.eggs_counted == NA_VALUE:
+            return ''
+        else:
+            return encounter.get_eggs_counted_display()
+
+    def dehydrate_hatchlings_measured(self, encounter):
+        if encounter.hatchlings_measured == NA_VALUE:
+            return ''
+        else:
+            return encounter.get_hatchlings_measured_display()
+
+    def dehydrate_fan_angles_measured(self, encounter):
+        if encounter.fan_angles_measured == NA_VALUE:
+            return ''
+        else:
+            return encounter.get_fan_angles_measured_display()
+
     class Meta:
         model = TurtleNestEncounter
-        fields = [
-            "source",
-            "source_id",
-            "where",
-            "wkt" "longitude",
-            "latitude",
-            "location_accuracy_m",
-            "when",
-            "area",
-            "area__name",
-            "site",
-            "site__name",
-            "observer",
-            "observer__name",
-            "reporter",
-            "reporter__name",
-            "encounter_type",
-            "species",
+        fields = EncounterResource.Meta.fields + [
             "nest_type",
             "nest_age",
-            "habitat",
+            "species",
             "disturbance",
             "nest_tagged",
             "logger_found",
