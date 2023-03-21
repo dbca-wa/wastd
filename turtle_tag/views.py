b10efae272bebdbab2df4ed1f9789684f7dc8fd6
Ashley Felton <ashley@ropable.com>
2023-03-21 09:41:51 +0800

    Refine turtle_tagging list & detail templates.

--- turtle_tag/views.py
+++ turtle_tag/views.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, DetailView
-from wastd.utils import search_filter
+#from wastd.utils import search_filter
 
 from .models import Turtle
 from .forms import TurtleSearchForm
@@ -23,17 +23,17 @@ class TurtleList(LoginRequiredMixin, ListView):
 
     def get_queryset(self):
         qs = super().get_queryset()
-        # NOTE: General-purpose search is too slow to use.
+        # General-purpose search uses the `q` parameter.
         #if 'q' in self.request.GET and self.request.GET['q']:
-        #    from .admin import TrtTurtlesAdmin
-        #    q = search_filter(TrtTurtlesAdmin.search_fields, self.request.GET['q'])
-        #    qs = qs.filter(q).distinct()
+        #    from .admin import TurtleAdmin
+        #    q = search_filter(TurtleAdmin.search_fields, self.request.GET['q'])
+        #    return qs.filter(q).distinct().order_by('pk')
         if 'turtle_id' in self.request.GET and self.request.GET['turtle_id']:
-            return qs.filter(pk=self.request.GET['turtle_id'])
+            return qs.filter(pk=self.request.GET['turtle_id']).order_by('pk')
         if 'tag_id' in self.request.GET and self.request.GET['tag_id']:
-            return qs.filter(tags__pk=self.request.GET['tag_id'])
+            return qs.filter(tags__pk=self.request.GET['tag_id']).order_by('pk')
         if 'pit_tag_id' in self.request.GET and self.request.GET['pit_tag_id']:
-            return qs.filter(pit_tags__pk=self.request.GET['pit_tag_id'])
+            return qs.filter(pit_tags__pk=self.request.GET['pit_tag_id']).order_by('pk')
         return qs
 
 
