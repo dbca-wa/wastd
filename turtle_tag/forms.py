3c72d76bfb1d9660563c38d7d0fc5e64504363bd
Ashley Felton <ashley@ropable.com>
2023-03-29 14:42:58 +0800

    Add basic view to create new Turtle records.

--- turtle_tag/forms.py
+++ turtle_tag/forms.py
@@ -1,6 +1,9 @@
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Layout, Submit, Field
+from crispy_forms.layout import Layout, Submit, Field, Div, HTML
+from crispy_forms.bootstrap import InlineCheckboxes
 from django import forms
+
+from .models import Turtle, TurtleObservation
 
 
 class TurtleSearchForm(forms.Form):
@@ -22,3 +25,77 @@ class TurtleSearchForm(forms.Form):
             Field('pit_tag_id', placeholder='Pit tag ID', css_class='mx-1'),
             Submit('search', 'Search', css_class='btn ml-1'),
         )
+
+
+class BaseFormHelper(FormHelper):
+    """Base FormHelper class, with common options set.
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.form_class = 'form-horizontal'
+        self.form_method = 'POST'
+        self.label_class = 'col-xs-12 col-sm-4 col-md-3 col-lg-2'
+        self.field_class = 'col-xs-12 col-sm-8 col-md-6 col-lg-4'
+        self.help_text_inline = True
+        self.attrs = {'novalidate': ''}
+
+
+class BaseForm(forms.ModelForm):
+    """Base ModelForm class for referral models.
+    """
+    save_button = Submit('save', 'Save and record tag(s)', css_class='btn-lg')
+    cancel_button = Submit('cancel', 'Cancel', css_class='btn-secondary')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = BaseFormHelper()
+
+
+class TurtleCreateForm(BaseForm):
+    FLIPPER_CHOICES = (
+        ('l1', 'Left flipper scale closest to body'),
+        ('l2', 'Left flipper scale 2nd from body'),
+        ('l3', 'Left flipper scale 3rd from body'),
+        ('r1', 'Right flipper scale closest to body'),
+        ('r2', 'Right flipper scale 2nd from body'),
+        ('r3', 'Right flipper scale 3rd from body'),
+    )
+    comments = forms.CharField(required=False)
+    prev_tags_lost = forms.BooleanField(required=False, label='All previous tags and IDs lost')
+    scars_not_checked = forms.BooleanField(required=False, label='Tag scars were not checked')
+    flipper_tag_scars = forms.MultipleChoiceField(choices=FLIPPER_CHOICES, required=False)
+
+    class Meta:
+        model = Turtle
+        fields = (
+            'species', 'sex', 'location', 'name', 'comments', 'prev_tags_lost', 'scars_not_checked',
+            'flipper_tag_scars',
+        )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Define the form layout.
+        self.helper.layout = Layout(
+            'species',
+            'sex',
+            'name',
+            'location',
+            'comments',
+            'prev_tags_lost',
+            'scars_not_checked',
+            Div(
+                HTML('Tick the scale(s) that have tag scars:'),
+            ),
+            InlineCheckboxes('flipper_tag_scars'),
+            Div(
+                self.save_button,
+                self.cancel_button,
+                css_class='offset-sm-4 offset-md-3 offset-lg-2 col-xs-12 col-sm-8 col-md-6 col-lg-4')
+        )
+
+
+class TurtleObservationCreateForm(BaseForm):
+
+    class Meta:
+        model = TurtleObservation
+        exclude = ('turtle', 'observation_date_old')
--- turtle_tag/views.py
+++ turtle_tag/views.py
@@ -1,10 +1,19 @@
 from django.conf import settings
+from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import ListView, DetailView
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, redirect
+from django.urls import reverse
+from django.views.generic import ListView, DetailView, CreateView
+from shared.utils import Breadcrumb
 from wastd.utils import search_filter
 
 from .models import Turtle, TurtleObservation
-from .forms import TurtleSearchForm
+from .forms import (
+    #TurtleSearchForm,
+    TurtleCreateForm,
+    TurtleObservationCreateForm,
+)
 
 
 class TurtleList(LoginRequiredMixin, ListView):
@@ -16,18 +25,22 @@ class TurtleList(LoginRequiredMixin, ListView):
         context["object_count"] = self.get_queryset().count()
         context["page_title"] = f"{settings.SITE_CODE} | Tagged turtles"
         # Pass in any query string
-        #if 'q' in self.request.GET:
-        #    context['query_string'] = self.request.GET['q']
-        context["search_form"] = TurtleSearchForm()
+        if 'q' in self.request.GET:
+            context['query_string'] = self.request.GET['q']
+        #context["search_form"] = TurtleSearchForm()
+        context["breadcrumbs"] = (
+            Breadcrumb("Home", reverse("home")),
+            Breadcrumb("Tagged turtles", None),
+        )
         return context
 
     def get_queryset(self):
         qs = super().get_queryset()
         # General-purpose search uses the `q` parameter.
-        #if 'q' in self.request.GET and self.request.GET['q']:
-        #    from .admin import TurtleAdmin
-        #    q = search_filter(TurtleAdmin.search_fields, self.request.GET['q'])
-        #    return qs.filter(q).distinct().order_by('pk')
+        if 'q' in self.request.GET and self.request.GET['q']:
+            from .admin import TurtleAdmin
+            q = search_filter(TurtleAdmin.search_fields, self.request.GET['q'])
+            return qs.filter(q).distinct().order_by('pk')
         if 'turtle_id' in self.request.GET and self.request.GET['turtle_id']:
             return qs.filter(pk=self.request.GET['turtle_id']).order_by('pk')
         if 'tag_id' in self.request.GET and self.request.GET['tag_id']:
@@ -55,6 +68,43 @@ class TurtleDetail(LoginRequiredMixin, DetailView):
         return context
 
 
+class TurtleCreate(LoginRequiredMixin, CreateView):
+    model = Turtle
+    form_class = TurtleCreateForm
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["page_title"] = f"{settings.SITE_CODE} | Create new turtle"
+        context["title"] = "Create new turtle"
+        context["breadcrumbs"] = (
+            Breadcrumb("Home", reverse("home")),
+            Breadcrumb("Tagged Turtles", reverse("turtle_tag:turtle_list")),
+            Breadcrumb("Create new turtle", None),
+        )
+        return context
+
+    def get_success_url(self):
+        # Turtle detail view.
+        return self.object.get_absolute_url()
+        # TODO: go to the "create tags" view
+        #return reverse("turtle_tags_update", kwargs={"pk": self.object.pk})
+
+    def post(self, request, *args, **kwargs):
+        # If the user clicked Cancel, redirect back to the turtle list view.
+        if request.POST.get("cancel"):
+            return redirect("turtle_tag:turtle_list")
+        return super().post(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        # Saves the form instance, sets the current object for the view,
+        # and redirects to get_success_url().
+        self.object = form.save(commit=False)
+        self.object.entered_by_person = self.request.user
+        self.object.save()
+        messages.success(self.request, "{} has been created.".format(self.object))
+        return HttpResponseRedirect(self.get_success_url())
+
+
 class TurtleObservationList(LoginRequiredMixin, ListView):
     model = TurtleObservation
     paginate_by = 50
@@ -66,7 +116,6 @@ class TurtleObservationList(LoginRequiredMixin, ListView):
         # Pass in any query string
         if 'q' in self.request.GET:
             context['query_string'] = self.request.GET['q']
-        #context["search_form"] = TurtleSearchForm()
         return context
 
     def get_queryset(self):
@@ -94,3 +143,30 @@ class TurtleObservationDetail(LoginRequiredMixin, DetailView):
         else:
             context["observation_points"] = None
         return context
+
+
+class TurtleObservationCreate(LoginRequiredMixin, CreateView):
+    model = TurtleObservation
+    form_class = TurtleObservationCreateForm
+    template_name = "turtle_tag/turtle_observation_create.html"
+
+    @property
+    def get_turtle(self):
+        return get_object_or_404(Turtle, pk=self.kwargs["pk"])
+
+    #def get_context_data(self, **kwargs):
+    #    pass
+
+    def post(self, request, *args, **kwargs):
+        # If the user clicked Cancel, redirect back to the turtle detail page.
+        if request.POST.get("cancel"):
+            return redirect(self.get_turtle().get_absolute_url())
+        return super().post(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.turtle = self.get_turtle()
+        return
+
+    def get_success_url(self):
+        pass
