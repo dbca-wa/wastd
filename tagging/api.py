567c3ea6161e673ad486365c85185fe95f720ee3
Ashley Felton <ashley@ropable.com>
2023-06-01 10:46:01 +0800

    Tweak behaviour of turtle observation tag select (limit API data return).

--- tagging/api.py
+++ tagging/api.py
@@ -12,6 +12,14 @@ class TurtleTagListResource(ListResourceView):
         # General-purpose filtering uses the `q` request parameter.
         if 'q' in self.request.GET and self.request.GET['q']:
             queryset = queryset.filter(serial__icontains=self.request.GET['q']).distinct()
+        # Where `existing_turtle_id` param is passed in, limit the queryset to tags
+        # assigned to that turtle OR unassigned tags.
+        # The use-case is the tag select on the turtle observation form, to prevent users
+        # from choosing tags assigned to a different turtle.
+        if 'existing_turtle_id' in self.request.GET and self.request.GET['existing_turtle_id']:
+            pk = int(self.request.GET['existing_turtle_id'])
+            queryset = queryset.filter(turtle__pk=pk) | queryset.filter(turtle__isnull=True)
+            queryset = queryset.distinct()
         if 'limit' in self.request.GET and self.request.GET['limit']:
             limit = int(self.request.GET['limit'])
             queryset = queryset[0:limit]
@@ -32,6 +40,14 @@ class TurtlePitTagListResource(ListResourceView):
         # General-purpose filtering uses the `q` request parameter.
         if 'q' in self.request.GET and self.request.GET['q']:
             queryset = queryset.filter(serial__icontains=self.request.GET['q']).distinct()
+        # Where `existing_turtle_id` param is passed in, limit the queryset to tags
+        # assigned to that turtle OR unassigned tags.
+        # The use-case is the tag select on the turtle observation form, to prevent users
+        # from choosing tags assigned to a different turtle.
+        if 'existing_turtle_id' in self.request.GET and self.request.GET['existing_turtle_id']:
+            pk = int(self.request.GET['existing_turtle_id'])
+            queryset = queryset.filter(turtle__pk=pk) | queryset.filter(turtle__isnull=True)
+            queryset = queryset.distinct()
         if 'limit' in self.request.GET and self.request.GET['limit']:
             limit = int(self.request.GET['limit'])
             queryset = queryset[0:limit]
