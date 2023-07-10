874b14f1220ddbb9feefac4aee7462b837439862
Ashley Felton <ashley@ropable.com>
2023-07-10 12:40:45 +0800

    Change STATUS_NEW to STATUS_IMPORTED.

--- observations/management/commands/automated_qa_checks.py
+++ observations/management/commands/automated_qa_checks.py
@@ -19,7 +19,7 @@ from users.models import User
 
 
 class Command(BaseCommand):
-    help = 'Runs automated QA/QC checks and flags records for curation'
+    help = 'Runs automated QA/QC checks to flag records for manual curation as needed'
 
     def handle(self, *args, **options):
         logger = logging.getLogger('turtles')
@@ -27,8 +27,7 @@ class Command(BaseCommand):
         system_user = User.objects.get(pk=1)
 
         # Check: Any turtle nest encounter with uncertain species.
-        # FIXME: STATUS_NEW may change to STATUS_IMPORTED.
-        nest_encounters = TurtleNestEncounter.objects.filter(species=TURTLE_SPECIES_DEFAULT, status=Encounter.STATUS_NEW)
+        nest_encounters = TurtleNestEncounter.objects.filter(species=TURTLE_SPECIES_DEFAULT, status=Encounter.STATUS_IMPORTED)
         if nest_encounters:
             logger.info(f'Flagging {nest_encounters.count()} turtle nest encounters for curation due to uncertain species')
         for enc in nest_encounters:
@@ -36,9 +35,8 @@ class Command(BaseCommand):
             enc.save()
 
         # Check: Any turtle nest encounter using 'test' species.
-        # FIXME: STATUS_NEW may change to STATUS_IMPORTED.
         for species in ["corolla-corolla", "test-turtle"]:
-            nest_encounters = TurtleNestEncounter.objects.filter(species=species, status=Encounter.STATUS_NEW)
+            nest_encounters = TurtleNestEncounter.objects.filter(species=species, status=Encounter.STATUS_IMPORTED)
             if nest_encounters:
                 logger.info(f'Flagging {nest_encounters.count()} turtle nest encounters for curation due to test species type')
             for enc in nest_encounters:
@@ -46,8 +44,7 @@ class Command(BaseCommand):
                 enc.save()
 
         # Check: Any turtle nest encounter with uncertain nesting outcome.
-        # FIXME: STATUS_NEW may change to STATUS_IMPORTED.
-        nest_encounters = TurtleNestEncounter.objects.filter(nest_type=NEST_TYPE_TRACK_UNSURE, status=Encounter.STATUS_NEW)
+        nest_encounters = TurtleNestEncounter.objects.filter(nest_type=NEST_TYPE_TRACK_UNSURE, status=Encounter.STATUS_IMPORTED)
         if nest_encounters:
             logger.info(f'Flagging {nest_encounters.count()} turtle nest encounters for curation due to uncertain nesting outcome')
         for enc in nest_encounters:
@@ -55,8 +52,7 @@ class Command(BaseCommand):
             enc.save()
 
         # Check: Any turtle nest encounter with uncertain nest age.
-        # FIXME: STATUS_NEW may change to STATUS_IMPORTED.
-        nest_encounters = TurtleNestEncounter.objects.filter(nest_age=NEST_AGE_DEFAULT, status=Encounter.STATUS_NEW)
+        nest_encounters = TurtleNestEncounter.objects.filter(nest_age=NEST_AGE_DEFAULT, status=Encounter.STATUS_IMPORTED)
         if nest_encounters:
             logger.info(f'Flagging {nest_encounters.count()} turtle nest encounters for curation due to uncertain nest age')
         for enc in nest_encounters:
@@ -73,7 +69,7 @@ class Command(BaseCommand):
                 nest_encounters.add(obs.encounter)
         pks = [enc.pk for enc in nest_encounters]
         # Convert back to a queryset
-        nest_encounters = TurtleNestEncounter.objects.filter(pk__in=pks, status=Encounter.STATUS_NEW)
+        nest_encounters = TurtleNestEncounter.objects.filter(pk__in=pks, status=Encounter.STATUS_IMPORTED)
         if nest_encounters:
             logger.info(f'Flagging {nest_encounters.count()} turtle nest encounters for curation due to uncertain predation')
         for enc in nest_encounters:
@@ -105,7 +101,7 @@ class Command(BaseCommand):
             species_name = el[1][1]
 
             for area in areas:
-                nest_encounters = TurtleNestEncounter.objects.filter(area=area, species=species, status=Encounter.STATUS_NEW)
+                nest_encounters = TurtleNestEncounter.objects.filter(area=area, species=species, status=Encounter.STATUS_IMPORTED)
                 if nest_encounters:
                     logger.info(f'Flagging {nest_encounters.count()} turtle nest encounters for curation: {species_name} at {area.name}')
                 for enc in nest_encounters:
@@ -121,7 +117,7 @@ class Command(BaseCommand):
                         nest_encounters.add(obs.encounter)
                 pks = [enc.pk for enc in nest_encounters]
                 # Convert back to a queryset
-                nest_encounters = TurtleNestEncounter.objects.filter(pk__in=pks, status=Encounter.STATUS_NEW)
+                nest_encounters = TurtleNestEncounter.objects.filter(pk__in=pks, status=Encounter.STATUS_IMPORTED)
                 if nest_encounters:
                     logger.info(f'Flagging {nest_encounters.count()} turtle nest encounters for curation: {species_name} at {area.name}')
                 for enc in nest_encounters:
@@ -147,7 +143,7 @@ class Command(BaseCommand):
 
             for area in areas:
                 # TurtleNestEncounter objects
-                nest_encounters = TurtleNestEncounter.objects.filter(area=area, species=species, status=Encounter.STATUS_NEW)
+                nest_encounters = TurtleNestEncounter.objects.filter(area=area, species=species, status=Encounter.STATUS_IMPORTED)
                 if nest_encounters:
                     logger.info(f'Flagging {nest_encounters.count()} turtle nest encounters for curation: {species_name} at {area.name}')
                 for enc in nest_encounters:
@@ -164,7 +160,7 @@ class Command(BaseCommand):
                         nest_encounters.add(obs.encounter)
                 pks = [enc.pk for enc in nest_encounters]
                 # Convert back to a queryset
-                nest_encounters = TurtleNestEncounter.objects.filter(pk__in=pks, status=Encounter.STATUS_NEW)
+                nest_encounters = TurtleNestEncounter.objects.filter(pk__in=pks, status=Encounter.STATUS_IMPORTED)
                 if nest_encounters:
                     logger.info(f'Flagging {nest_encounters.count()} turtle nest encounters for curation: {species_name} at {area.name}')
                 for enc in nest_encounters:
@@ -172,8 +168,7 @@ class Command(BaseCommand):
                     enc.save()
 
         # Mark remaining TurtleNestEncounter objects with status == New as imported (passed QA/QC checks).
-        # FIXME: STATUS_NEW may change to STATUS_IMPORTED.
-        nest_encounters = TurtleNestEncounter.objects.filter(status=Encounter.STATUS_NEW)
+        nest_encounters = TurtleNestEncounter.objects.filter(status=Encounter.STATUS_IMPORTED)
         if nest_encounters:
             logger.info(f'Marking {nest_encounters.count()} imported turtle nest encounters as curated (passed QA/QC checks)')
         for enc in nest_encounters:
