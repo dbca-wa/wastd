ab2e9b8c80e0e062f9249f48aeee5dc29af30de2
Ashley Felton <ashley@ropable.com>
2023-07-28 15:45:21 +0800

    Strip out most unit test classes.

--- observations/tests/test_functional.py
+++
@@ -1,7 +0,0 @@
-# Test Campaigns
-# Given: three surveys, three Enc.
-# Create Campaign. One svy and enc are outside camp.
-# Expect the svys and encs inside cmapaign to be linked to camp.
-# Expect the svy and enc outside camp not to be linked to camp.
-# Adjust camp to make one svy and enc fall outside.
-# Create new camp2 to capture the expelled svy and enc. Expect those to be linked to camp2.
