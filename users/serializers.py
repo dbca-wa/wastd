755b9358f1fcfff4668c2025666d2098d8d87875
Ashley Felton <ashley@ropable.com>
2023-08-11 14:12:20 +0800

    Tweak and simplify v2 API endpoints.

--- users/serializers.py
+++ users/serializers.py
@@ -2,16 +2,6 @@ from typing import Dict, Any
 from .models import User
 
 
-def user_serializer_basic(user: User) -> Dict[str, Any]:
-    """Function to return minimal attributes of a passed-in User as a simple dict.
-    """
-    return {
-        'id': user.pk,
-        'username': user.username,
-        'name': user.name,
-    }
-
-
 def user_serializer(user: User) -> Dict[str, Any]:
     """Function to return attributes of a passed-in User as a simple dict.
     """
