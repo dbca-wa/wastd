47cfa53d3e5eedfd77d9d7da8d3a75e77c87bfb8
Evan Hallein <ehallein@icloud.com>
2023-10-11 10:12:28 +0800

    Added django debug toolbar package

--- wastd/settings.py
+++ wastd/settings.py
@@ -72,6 +72,7 @@ INSTALLED_APPS = [
     "wamtram",  # Legacy WAMTRAM database
     #"tagging",  # Temporary turtle tagging data
     "turtle_tags",
+    "debug_toolbar",
 ]
 
 MIDDLEWARE = [
@@ -80,12 +81,29 @@ MIDDLEWARE = [
     "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "reversion.middleware.RevisionMiddleware",
     "dbca_utils.middleware.SSOLoginMiddleware",
+]
+DEBUG_TOOLBAR_PANELS = [
+    'debug_toolbar.panels.history.HistoryPanel',
+    'debug_toolbar.panels.versions.VersionsPanel',
+    'debug_toolbar.panels.timer.TimerPanel',
+    'debug_toolbar.panels.settings.SettingsPanel',
+    'debug_toolbar.panels.headers.HeadersPanel',
+    'debug_toolbar.panels.request.RequestPanel',
+    'debug_toolbar.panels.sql.SQLPanel',
+    'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+    'debug_toolbar.panels.templates.TemplatesPanel',
+    'debug_toolbar.panels.cache.CachePanel',
+    'debug_toolbar.panels.signals.SignalsPanel',
+    'debug_toolbar.panels.redirects.RedirectsPanel',
+    'debug_toolbar.panels.profiling.ProfilingPanel',
+    "wastd.middleware.FileInterceptsPanel"
 ]
 
 TEMPLATES = [
