bce689714efb9fd1ec346b0bfffd663e23ff0265
Ashley Felton <ashley@ropable.com>
2024-04-23 13:42:56 +0800

    App name

--- marine_mammal_incidents/apps.py
+++ marine_mammal_incidents/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 class MarineMammalIncidentsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'marine_mammal_incidents'
-    verbose_name = 'marine mammal incidents'
+    verbose_name = 'Marine mammal incidents'
