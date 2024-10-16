ff89b4cd43e657adfa490c7bb0cf763081bab402
Xinlyu Wang <xinlalalele@gmail.com>
2024-10-16 13:31:15 +0800

    Adjust the size for comment area

--- marine_mammal_incidents/forms.py
+++ marine_mammal_incidents/forms.py
@@ -3,6 +3,11 @@ from .models import Incident, Uploaded_file
 from mapwidgets.widgets import MapboxPointFieldWidget
 
 class IncidentForm(forms.ModelForm):
+    comments = forms.CharField(
+        widget=forms.Textarea(attrs={'cols': '100', 'rows': '10'}),
+        required=False
+    )
+
     class Meta:
         model = Incident
         fields = '__all__'
