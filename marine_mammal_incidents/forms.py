634446b4c136cf590635924e559701be0dcf172b
Xinlyu Wang <xinlalalele@gmail.com>
2024-10-16 09:52:39 +0800

    Create Incident view

---
+++ marine_mammal_incidents/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from .models import Incident, Uploaded_file
+from mapwidgets.widgets import MapboxPointFieldWidget
+
+class IncidentForm(forms.ModelForm):
+    class Meta:
+        model = Incident
+        fields = '__all__'
+        widgets = {
+            'geo_location': MapboxPointFieldWidget(),
+        }
+
+class UploadedFileForm(forms.ModelForm):
+    class Meta:
+        model = Uploaded_file
+        fields = ['title', 'file']
--- marine_mammal_incidents/views.py
+++ marine_mammal_incidents/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.forms import inlineformset_factory
+from .models import Incident, Uploaded_file
+from .forms import IncidentForm, UploadedFileForm
 
-# Create your views here.
+def create_incident(request):
+    UploadedFileFormSet = inlineformset_factory(Incident, Uploaded_file, form=UploadedFileForm, extra=1)
+    
+    if request.method == 'POST':
+        form = IncidentForm(request.POST)
+        formset = UploadedFileFormSet(request.POST, request.FILES)
+        if form.is_valid() and formset.is_valid():
+            incident = form.save()
+            formset.instance = incident
+            formset.save()
+            return redirect('incident_list')  # 假设您有一个名为'incident_list'的URL模式
+    else:
+        form = IncidentForm()
+        formset = UploadedFileFormSet()
+    
+    return render(request, 'marine_mammal_incidents/create_incident.html', {
+        'form': form,
+        'formset': formset,
+    })
