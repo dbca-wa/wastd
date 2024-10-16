59b8b48e07e89cfb992fa9e5429a30c26af1b2d2
Xinlyu Wang <xinlalalele@gmail.com>
2024-10-16 10:07:19 +0800

    Create incidents list

--- marine_mammal_incidents/urls.py
+++ marine_mammal_incidents/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('create/', views.create_incident, name='create_incident'),
+    path('list/', views.incident_list, name='incident_list'),
 ]
--- marine_mammal_incidents/views.py
+++ marine_mammal_incidents/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.forms import inlineformset_factory
 from .models import Incident, Uploaded_file
 from .forms import IncidentForm, UploadedFileForm
+from django.contrib import messages
 
 def create_incident(request):
     UploadedFileFormSet = inlineformset_factory(Incident, Uploaded_file, form=UploadedFileForm, extra=1)
@@ -13,12 +14,23 @@ def create_incident(request):
             incident = form.save()
             formset.instance = incident
             formset.save()
-            return redirect('incident_list')  # 假设您有一个名为'incident_list'的URL模式
+            messages.success(request, 'Incident created successfully')
+            return redirect('marine_mammal_incidents:incident_list')
+        else:
+            messages.error(request, 'Error creating incident')
     else:
         form = IncidentForm()
         formset = UploadedFileFormSet()
     
-    return render(request, 'marine_mammal_incidents/create_incident.html', {
+    context = {
         'form': form,
         'formset': formset,
-    })
+    }
+    return render(request, 'marine_mammal_incidents/create_incident.html', context)
+
+def incident_list(request):
+    incidents = Incident.objects.all().order_by('-incident_date')
+    context = {
+        'incidents': incidents,
+    }
+    return render(request, 'marine_mammal_incidents/incident_list.html', context)
