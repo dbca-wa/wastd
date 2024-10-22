7c3db0cd1d5631583cbaf4f3ad30b85c59ebcc96
Xinlyu Wang <xinlalalele@gmail.com>
2024-10-22 11:22:30 +0800

    Add export feature for mammal incidents

--- marine_mammal_incidents/urls.py
+++ marine_mammal_incidents/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('create/', views.incident_form, name='create_incident'),
     path('list/', views.incident_list, name='incident_list'),
     path('<int:pk>/update/', views.incident_form, name='update_incident'),
+    path('export/', views.export_form, name='export_form'),
+    path('export/data/', views.export_data, name='export_data'),
 ]
--- marine_mammal_incidents/views.py
+++ marine_mammal_incidents/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.forms import inlineformset_factory
-from .models import Incident, Uploaded_file
+from .models import Incident, Uploaded_file, Species
 from .forms import IncidentForm, UploadedFileForm
 from django.contrib import messages
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+import csv
+import xlwt
+from openpyxl import Workbook
+from django.http import HttpResponse
 
 
 
@@ -61,4 +65,105 @@ def incident_list(request):
     }
     return render(request, 'marine_mammal_incidents/incident_list.html', context)
 
+def export_form(request):
+    species_list = Species.objects.all()
+    return render(request, 'marine_mammal_incidents/export_form.html', {'species_list': species_list})
 
+
+
+
+def export_data(request):
+
+    incident_date_from = request.GET.get('incident_date_from')
+    incident_date_to = request.GET.get('incident_date_to')
+    species_id = request.GET.get('species')
+    location_name = request.GET.get('location_name')
+    file_format = request.GET.get('format', 'csv')
+
+    incidents = Incident.objects.all()
+    if incident_date_from:
+        incidents = incidents.filter(incident_date__gte=incident_date_from)
+    if incident_date_to:
+        incidents = incidents.filter(incident_date__lte=incident_date_to)
+    if species_id:
+        incidents = incidents.filter(species_id=species_id)
+    if location_name:
+        incidents = incidents.filter(location_name__icontains=location_name)
+
+
+    data = [['事件日期', '物种', '位置名称', '事件类型', '动物数量']]
+    for incident in incidents:
+        data.append([
+            incident.incident_date,
+            str(incident.species),
+            incident.location_name,
+            incident.incident_type,
+            incident.number_of_animals
+        ])
+
+    # Prepare data
+    data = [['Species', 'Latitude', 'Longitude', 'ID', 'Incident Date', 'Incident Time', 
+            'Species Confirmed Genetically', 'Location Name', 'Geo Location', 
+            'Number of Animals', 'Mass Incident', 'Incident Type', 'Sex', 'Age Class', 
+            'Length', 'Weight', 'Weight Is Estimated', 'Carcass Location Fate', 
+            'Entanglement Gear', 'DBCA Staff Attended', 'Condition When Found', 
+            'Outcome', 'Cause of Death', 'Photos Taken', 'Samples Taken', 
+            'Post Mortem', 'Comments']]
+
+    for incident in incidents:
+        data.append([
+            str(incident.species) if incident.species is not None else '',
+            incident.latitude if incident.latitude is not None else '',
+            incident.longitude if incident.longitude is not None else '',
+            incident.id if incident.id is not None else '',
+            incident.incident_date if incident.incident_date is not None else '',
+            incident.incident_time if incident.incident_time is not None else '',
+            incident.species_confirmed_genetically if incident.species_confirmed_genetically is not None else '',
+            incident.location_name if incident.location_name is not None else '',
+            incident.geo_location if incident.geo_location is not None else '',
+            incident.number_of_animals if incident.number_of_animals is not None else '',
+            incident.mass_incident if incident.mass_incident is not None else '',
+            incident.incident_type if incident.incident_type is not None else '',
+            incident.sex if incident.sex is not None else '',
+            incident.age_class if incident.age_class is not None else '',
+            incident.length if incident.length is not None else '',
+            incident.weight if incident.weight is not None else '',
+            incident.weight_is_estimated if incident.weight_is_estimated is not None else '',
+            incident.carcass_location_fate if incident.carcass_location_fate is not None else '',
+            incident.entanglement_gear if incident.entanglement_gear is not None else '',
+            incident.DBCA_staff_attended if incident.DBCA_staff_attended is not None else '',
+            incident.condition_when_found if incident.condition_when_found is not None else '',
+            incident.outcome if incident.outcome is not None else '',
+            incident.cause_of_death if incident.cause_of_death is not None else '',
+            incident.photos_taken if incident.photos_taken is not None else '',
+            incident.samples_taken if incident.samples_taken is not None else '',
+            incident.post_mortem if incident.post_mortem is not None else '',
+            incident.comments if incident.comments is not None else ''
+        ])
+
+    if file_format == 'csv':
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename="incidents.csv"'
+        writer = csv.writer(response)
+        writer.writerows(data)
+    elif file_format == 'xls':
+        response = HttpResponse(content_type='application/ms-excel')
+        response['Content-Disposition'] = 'attachment; filename="incidents.xls"'
+        wb = xlwt.Workbook(encoding='utf-8')
+        ws = wb.add_sheet('Incidents')
+        for row_num, row_data in enumerate(data):
+            for col_num, cell_value in enumerate(row_data):
+                ws.write(row_num, col_num, cell_value)
+        wb.save(response)
+    elif file_format == 'xlsx':
+        response = HttpResponse(content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+        response['Content-Disposition'] = 'attachment; filename="incidents.xlsx"'
+        wb = Workbook()
+        ws = wb.active
+        for row in data:
+            ws.append(row)
+        wb.save(response)
+    else:
+        return HttpResponse("Invalid file format")
+
+    return response
