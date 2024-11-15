e0bfb949a344977100e10372a1b708df0a0a8b8a
Rick Wang <xinlyuwang96@gmail.com>
2024-11-15 09:18:13 +0800

    Fix batch curation list view

--- wamtram2/urls.py
+++ wamtram2/urls.py
@@ -51,5 +51,5 @@ urlpatterns = [
     path('curation/transfer-observations-by-tag/', views.TransferObservationsByTagView.as_view(), name='transfer_observations_by_tag'),
     path('curation/nesting-seasons/', views.NestingSeasonListView.as_view(), name='nesting_season_list'),
     path('curation/batches/', views.BatchGridView.as_view(), name='batch_grid'),
-    path('curation/batch/<int:batch_id>/entries/', views.BatchEntriesView.as_view(), name='batch_entries'),  
+    path('curation/batch/<int:batch_id>/entries/', views.BatchGridView.as_view(), name='batch_entries'),  
 ]
