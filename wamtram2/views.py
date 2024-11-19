b826dfdec31c00848cf169dbe9f739577d81d483
Rick Wang <xinlyuwang96@gmail.com>
2024-11-19 10:52:16 +0800

    Fix bug in entry curation list page

--- wamtram2/views.py
+++ wamtram2/views.py
@@ -3483,152 +3483,165 @@ class EntryCurationView(LoginRequiredMixin, PaginateMixin, ListView):
         return queryset
     
     def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        
-        context.update({
-            'species_choices': TrtSpecies.objects.all(),
-            'places_choices': TrtPlaces.objects.all(),
-            'activities_choices': TrtActivities.objects.all(),
-            'yesno_choices': TrtYesNo.objects.all(),
-            'persons_choices': TrtPersons.objects.all(),
-            'damage_codes_choices': TrtDamageCodes.objects.all(),
-            'body_parts_choices': TrtBodyParts.objects.all(),
-            'measurement_types_choices': TrtMeasurementTypes.objects.all(),
-            'tag_states_choices': TrtTagStates.objects.all(),
-            'tissue_types_choices': TrtTissueTypes.objects.all(),
-            'egg_count_methods_choices': TrtEggCountMethods.objects.all(),
-            'identification_types_choices': TrtIdentificationTypes.objects.all(),
-            'tags_choices': TrtTags.objects.all(),
-            'pit_tags_choices': TrtPitTags.objects.all(),
-        })
-        
-        model_fields = TrtDataEntry._meta.get_fields()
-    
-        all_columns = []
-        
-        default_visible_fields = {
-            'data_entry_id', 'species_code', 'place_code', 
-            'observation_date', 'do_not_process', 'curved_carapace_length',
-            'curved_carapace_width', 'alive', 'activity_code', 'nesting'
-        }
-        
-        field_groups = {
-            'Basic Information': [
-                'data_entry_id', 'species_code', 'place_code', 
-                'observation_date', 'observation_time', 'do_not_process'
-            ],
-            'Measurements': [
-                'curved_carapace_length', 'curved_carapace_width', 
-                'curved_carapace_length_notch', 'cc_length_not_measured',
-                'cc_width_not_measured', 'cc_notch_length_not_measured',
-                'measurement_type_1', 'measurement_value_1',
-                'measurement_type_2', 'measurement_value_2',
-                'measurement_type_3', 'measurement_value_3',
-                'measurement_type_4', 'measurement_value_4',
-                'measurement_type_5', 'measurement_value_5',
-                'measurement_type_6', 'measurement_value_6'
-            ],
-            'Status and Activities': [
-                'alive', 'activity_code', 'nesting', 'interrupted',
-                'identification_confidence'
-            ],
-            'Tags': [
-                'recapture_left_tag_id', 'recapture_left_tag_id_2', 'recapture_left_tag_id_3',
-                'recapture_right_tag_id', 'recapture_right_tag_id_2', 'recapture_right_tag_id_3',
-                'recapture_pittag_id', 'recapture_pittag_id_2', 'recapture_pittag_id_3', 'recapture_pittag_id_4',
-                'new_left_tag_id', 'new_left_tag_id_2',
-                'new_right_tag_id', 'new_right_tag_id_2',
-                'new_pittag_id', 'new_pittag_id_2', 'new_pittag_id_3', 'new_pittag_id_4',
-                'other_left_tag', 'other_right_tag', 'other_tags',
-                'other_tags_identification_type',
-                'dud_flipper_tag', 'dud_flipper_tag_2',
-                'dud_pit_tag', 'dud_pit_tag_2'
-            ],
-            'Tag States': [
-                'recapture_left_tag_state', 'recapture_left_tag_state_2',
-                'recapture_right_tag_state', 'recapture_right_tag_state_2',
-                'new_left_tag_state', 'new_left_tag_state_2',
-                'new_right_tag_state', 'new_right_tag_state_2'
-            ],
-            'PIT Tag Stickers': [
-                'new_pit_tag_sticker_present', 'new_pit_tag_2_sticker_present',
-                'new_pit_tag_3_sticker_present', 'new_pit_tag_4_sticker_present'
-            ],
-            'Checks': [
-                'flipper_tag_check', 'pit_tag_check', 'injury_check', 'scar_check',
-                'tagscarnotchecked', 'didnotcheckforinjury'
-            ],
-            'Damage and Scars': [
-                'damage_carapace', 'damage_lff', 'damage_rff', 'damage_lhf', 'damage_rhf',
-                'body_part_1', 'damage_code_1',
-                'body_part_2', 'damage_code_2',
-                'body_part_3', 'damage_code_3',
-                'scars_left', 'scars_right',
-                'scars_left_scale_1', 'scars_left_scale_2', 'scars_left_scale_3',
-                'scars_right_scale_1', 'scars_right_scale_2', 'scars_right_scale_3'
-            ],
-            'Tissue Samples': [
-                'tissue_type_1', 'sample_label_1',
-                'tissue_type_2', 'sample_label_2'
-            ],
-            'Nesting Data': [
-                'egg_count', 'egg_count_method', 'clutch_completed'
-            ],
-            'Personnel': [
-                'measured_by_id', 'recorded_by_id', 'tagged_by_id',
-                'entered_by_id', 'measured_recorded_by_id'
-            ],
-            'Comments': [
-                'comments', 'turtle_comments', 'comment_fromrecordedtagstable',
-                'error_message', 'error_number'
-            ]
-        }
-        
-
-        processed_fields = set()
-        
-        for group_name, field_patterns in field_groups.items():
-            group_fields = []
-            for field in model_fields:
-                if hasattr(field, 'name'): 
-                    field_name = field.name
-                
-                    if any(pattern in field_name for pattern in field_patterns) or field_name in field_patterns:
-                        group_fields.append({
-                            'field': field_name,
-                            'title': field_name.replace('_', ' ').title(),
-                            'visible': field_name in default_visible_fields,
-                            'group': group_name
-                        })
-                        processed_fields.add(field_name)
+            context = super().get_context_data(**kwargs)
             
-
-            all_columns.extend(sorted(group_fields, key=lambda x: x['field']))
-        
-
-        for field in model_fields:
-            if hasattr(field, 'name') and field.name not in processed_fields:
-                all_columns.append({
-                    'field': field.name,
-                    'title': field.name.replace('_', ' ').title(),
-                    'visible': field.name in default_visible_fields,
-                    'group': 'Other'
-                })
-        
-        user_columns = self.request.session.get('entry_grid_columns', 
-                                            [col['field'] for col in all_columns if col['visible']])
-        
-        batch_id = self.kwargs.get('batch_id')
-        
-        context.update({
-            'all_columns': all_columns,
-            'visible_columns': user_columns,
-            'search_term': self.request.GET.get('search', ''),
-            'clear_url': reverse('wamtram2:batch_entries', kwargs={'batch_id': batch_id}),
-            'batch_id': batch_id,
-        })
-        return context
-
+            context.update({
+                'species_choices': TrtSpecies.objects.all(),
+                'places_choices': TrtPlaces.objects.select_related('location_code').all(),
+                'activities_choices': TrtActivities.objects.all(),
+                'yesno_choices': TrtYesNo.objects.all(),
+                'persons_choices': TrtPersons.objects.all(),
+                'damage_codes_choices': TrtDamageCodes.objects.all(),
+                'body_parts_choices': TrtBodyParts.objects.all(),
+                'measurement_types_choices': TrtMeasurementTypes.objects.all(),
+                'tag_states_choices': TrtTagStates.objects.all(),
+                'tissue_types_choices': TrtTissueTypes.objects.all(),
+                'egg_count_methods_choices': TrtEggCountMethods.objects.all(),
+                'identification_types_choices': TrtIdentificationTypes.objects.all(),
+                'tags_choices': TrtTags.objects.all(),
+                'pit_tags_choices': TrtPitTags.objects.all(),
+            })
+            
+            model_fields = TrtDataEntry._meta.get_fields()
+    
+            default_visible_fields = {
+                'data_entry_id', 'species_code', 'place_code',
+                'observation_date', 'do_not_process', 'curved_carapace_length',
+                'curved_carapace_width', 'alive', 'activity_code', 'nesting'
+            }
+            
+            field_groups = {
+                'Basic Information': [
+                    'data_entry_id', 'species_code', 'place_code',
+                    'observation_date', 'observation_time', 'do_not_process'
+                ],
+                'Measurements': [
+                    'curved_carapace_length', 'curved_carapace_width',
+                    'curved_carapace_length_notch', 'cc_length_not_measured',
+                    'cc_width_not_measured', 'cc_notch_length_not_measured',
+                    'measurement_type_1', 'measurement_value_1',
+                    'measurement_type_2', 'measurement_value_2',
+                    'measurement_type_3', 'measurement_value_3',
+                    'measurement_type_4', 'measurement_value_4',
+                    'measurement_type_5', 'measurement_value_5',
+                    'measurement_type_6', 'measurement_value_6'
+                ],
+                'Status and Activities': [
+                    'alive', 'activity_code', 'nesting', 'interrupted',
+                    'identification_confidence'
+                ],
+                'Tags': [
+                    'recapture_left_tag_id', 'recapture_left_tag_id_2', 'recapture_left_tag_id_3',
+                    'recapture_right_tag_id', 'recapture_right_tag_id_2', 'recapture_right_tag_id_3',
+                    'recapture_pittag_id', 'recapture_pittag_id_2', 'recapture_pittag_id_3', 'recapture_pittag_id_4',
+                    'new_left_tag_id', 'new_left_tag_id_2',
+                    'new_right_tag_id', 'new_right_tag_id_2',
+                    'new_pittag_id', 'new_pittag_id_2', 'new_pittag_id_3', 'new_pittag_id_4',
+                    'other_left_tag', 'other_right_tag', 'other_tags',
+                    'other_tags_identification_type',
+                    'dud_flipper_tag', 'dud_flipper_tag_2',
+                    'dud_pit_tag', 'dud_pit_tag_2'
+                ],
+                'Tag States': [
+                    'recapture_left_tag_state', 'recapture_left_tag_state_2',
+                    'recapture_right_tag_state', 'recapture_right_tag_state_2',
+                    'new_left_tag_state', 'new_left_tag_state_2',
+                    'new_right_tag_state', 'new_right_tag_state_2'
+                ],
+                'PIT Tag Stickers': [
+                    'new_pit_tag_sticker_present', 'new_pit_tag_2_sticker_present',
+                    'new_pit_tag_3_sticker_present', 'new_pit_tag_4_sticker_present'
+                ],
+                'Checks': [
+                    'flipper_tag_check', 'pit_tag_check', 'injury_check', 'scar_check',
+                    'tagscarnotchecked', 'didnotcheckforinjury'
+                ],
+                'Damage and Scars': [
+                    'damage_carapace', 'damage_lff', 'damage_rff', 'damage_lhf', 'damage_rhf',
+                    'body_part_1', 'damage_code_1',
+                    'body_part_2', 'damage_code_2',
+                    'body_part_3', 'damage_code_3',
+                    'scars_left', 'scars_right',
+                    'scars_left_scale_1', 'scars_left_scale_2', 'scars_left_scale_3',
+                    'scars_right_scale_1', 'scars_right_scale_2', 'scars_right_scale_3'
+                ],
+                'Tissue Samples': [
+                    'tissue_type_1', 'sample_label_1',
+                    'tissue_type_2', 'sample_label_2'
+                ],
+                'Nesting Data': [
+                    'egg_count', 'egg_count_method', 'clutch_completed'
+                ],
+                'Personnel': [
+                    'measured_by_id', 'recorded_by_id', 'tagged_by_id',
+                    'entered_by_id', 'measured_recorded_by_id'
+                ],
+                'Comments': [
+                    'comments', 'turtle_comments', 'comment_fromrecordedtagstable',
+                    'error_message', 'error_number'
+                ]
+            }
+    
+    
+            field_order = {}
+            order_index = 0
+            for group_fields in field_groups.values():
+                for field in group_fields:
+                    field_order[field] = order_index
+                    order_index += 1
+    
+            all_columns = []
+            processed_fields = set()
+    
+    
+            for group_name, field_patterns in field_groups.items():
+                group_fields = []
+                for field in model_fields:
+                    if hasattr(field, 'name'):
+                        field_name = field.name
+                        if field_name in field_patterns:
+                            group_fields.append({
+                                'field': field_name,
+                                'title': field_name.replace('_', ' ').title(),
+                                'visible': field_name in default_visible_fields,
+                                'group': group_name
+                            })
+                            processed_fields.add(field_name)
+                
+            
+                sorted_fields = sorted(group_fields, key=lambda x: field_order.get(x['field'], float('inf')))
+                all_columns.extend(sorted_fields)
+    
+    
+            other_fields = []
+            for field in model_fields:
+                if hasattr(field, 'name') and field.name not in processed_fields:
+                    other_fields.append({
+                        'field': field.name,
+                        'title': field.name.replace('_', ' ').title(),
+                        'visible': field.name in default_visible_fields,
+                        'group': 'Other'
+                    })
+            
+    
+            all_columns.extend(sorted(other_fields, key=lambda x: x['field']))
+    
+    
+            user_columns = self.request.session.get('entry_grid_columns',
+                                                [col['field'] for col in all_columns if col['visible']])
+            
+            batch_id = self.kwargs.get('batch_id')
+            
+    
+            context.update({
+                'all_columns': all_columns,
+                'visible_columns': user_columns,
+                'search_term': self.request.GET.get('search', ''),
+                'clear_url': reverse('wamtram2:batch_entries', kwargs={'batch_id': batch_id}),
+                'batch_id': batch_id,
+            })
+            
+            return context
     def post(self, request, *args, **kwargs):
         if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
             visible_columns = request.POST.getlist('columns[]')
