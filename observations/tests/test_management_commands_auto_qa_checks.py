536b92012527475cef3400d97936b12938d0ac09
Xinlyu Rick Wang <xinlyuwang96@gmail.com>
2024-11-28 14:55:42 +0800

    Update test_management_commands_auto_qa_checks.py

--- observations/tests/test_management_commands_auto_qa_checks.py
+++ observations/tests/test_management_commands_auto_qa_checks.py
@@ -275,6 +275,7 @@ class AutomatedQAChecksCommandTests(TestCase):
         )
 
         call_command('automated_qa_checks')
+        print(mock_logger.info.call_args_list)
         mock_logger.info.assert_any_call('Flagging 1 turtle nest encounters for curation due to unknown reporter')
 
     @patch('logging.getLogger')
