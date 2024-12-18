198a8c04834161ab0d0f98954427a72c0dee7159
Xinlyu Wang <xinlalalele@gmail.com>
2024-12-18 21:31:58 +0800

    Import bacpac data to local sql server

---
+++ init-sqlserver.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+apt-get update
+apt-get install -y unzip curl libicu70
+
+curl -sSL -o sqlpackage.zip https://aka.ms/sqlpackage-linux
+mkdir -p /opt/sqlpackage
+unzip -q sqlpackage.zip -d /opt/sqlpackage
+chmod +x /opt/sqlpackage/sqlpackage
+
+/opt/sqlpackage/sqlpackage \
+    /Action:Import \
+    /SourceFile:/backup/turtle_tagging_uat-2024-10-4-10-27.bacpac \
+    /TargetServerName:sqlserver \
+    /TargetDatabaseName:turtle_tagging_uat \
+    /TargetUser:sa \
+    /TargetPassword:"$SA_PASSWORD" \
+    /TargetTrustServerCertificate:true
