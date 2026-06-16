3fe0468942aa83440e14f8ccbad0da19f0111878
Ashley Felton <ashley@ropable.com>
2026-06-16 12:04:58 +0800

    Fix for django-leaflet marker image path

--- wastd/settings.py
+++ wastd/settings.py
@@ -263,7 +263,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "/static/"
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "wastd", "static"),)
 WHITENOISE_ROOT = STATIC_ROOT
-WHITENOISE_MANIFEST_STRICT = False
 
 # Media (user-uploaded files)
 MEDIA_URL = "/media/"
