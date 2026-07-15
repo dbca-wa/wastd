97ed391733ac80eca1981c8d0977a62911850444
Liz Huangfu <lizhuangfu@dbca.wa.gov.au>
2026-07-15 11:20:54 +0800

    Add module access middleware and navigation controls

--- wastd/settings.py
+++ wastd/settings.py
@@ -109,6 +109,7 @@ MIDDLEWARE = [
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "users.middleware.ModuleAccessMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "reversion.middleware.RevisionMiddleware",
