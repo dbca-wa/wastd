7945d3e19c5774da62315181c85f3280f9fda367
Liz Huangfu <lizhuangfu@dbca.wa.gov.au>
2026-08-25 15:00:47 +0800

    feat(T074): add deletion logging in user admin

--- users/admin.py
+++ users/admin.py
@@ -1,8 +1,9 @@
 from django import forms
-from django.contrib import admin
+from django.contrib import admin,messages
 from django.contrib.auth.admin import UserAdmin as AuthUserAdmin
 from django.contrib.auth.forms import UserChangeForm, UserCreationForm
-
+from datetime import timedelta
+from django.utils import timezone
 from .models import AuditLog, User, Organisation
 
 
@@ -118,8 +119,22 @@ class UserAdmin(AuthUserAdmin):
                 return []
             else:
                 return [f.name for f in self.model._meta.fields]
+
+@admin.action(description="Delete all audit logs older than 1 year")
+
+def delete_audit_logs_older_than_one_year(modeladmin, request, queryset):
+    cutoff = timezone.now() - timedelta(days=365)
+    deleted_count, _ = AuditLog.objects.filter(created_at__lt=cutoff).delete()
+    modeladmin.message_user(
+        request,
+        f"Deleted {deleted_count} audit logs older than 1 year.",
+        messages.SUCCESS,
+    )
+
+
 @admin.register(AuditLog)
 class AuditLogAdmin(admin.ModelAdmin):
+
     list_display = (
         "created_at",
         "action",
@@ -130,6 +145,7 @@ class AuditLogAdmin(admin.ModelAdmin):
         "object_repr",
         "path",
     )
+
     list_filter = ("action", "app_label", "model_name", "created_at")
     search_fields = ("actor__username", "actor__email", "object_pk", "object_repr", "path")
     readonly_fields = (
@@ -145,6 +161,11 @@ class AuditLogAdmin(admin.ModelAdmin):
         "remote_addr",
     )
 
+    date_hierarchy = "created_at"
+    ordering = ("-created_at",)
+    list_per_page = 50
+    actions = (delete_audit_logs_older_than_one_year,)
+
     def has_add_permission(self, request):
         return False
 
@@ -152,4 +173,9 @@ class AuditLogAdmin(admin.ModelAdmin):
         return False
 
     def has_delete_permission(self, request, obj=None):
-        return False
+        return request.user.is_superuser
+
+    def get_actions(self, request):
+        actions = super().get_actions(request)
+        actions.pop("delete_selected", None)
+        return actions
